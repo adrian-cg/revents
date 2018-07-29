96ac5ebef7e361d6660095f4bfb39588c4e1346e
Adrian Castellanos <adriancg@gmail.com>
2018-07-29 03:40:28 -0500

    Finished Section 6

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
@@ -8,15 +9,20 @@ import 'semantic-ui-css/semantic.min.css';
 const rootEl = document.getElementById('root');
 
 const render = () => {
-  ReactDOM.render(<App />, rootEl);
-}
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootEl
+  );
+};
 
-if(module.hot) {
+if (module.hot) {
   module.hot.accept('./app/layout/App', () => {
     setTimeout(() => {
       render();
     });
-  })
+  });
 }
 render();
 registerServiceWorker();

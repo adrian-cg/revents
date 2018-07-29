6741e12c30b35643c23ab2c157be9918686a2ed4
Adrian Castellanos <adriancg@gmail.com>
2018-07-29 18:32:58 -0500

    Finished Section 7

---
+++ src/features/testarea/TestComponent.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { incrementCounter, decrementCounter } from './testActions';
+import { Button } from 'semantic-ui-react/dist/commonjs';
+
+const mapStateToProps = state => ({
+  data: state.test.data
+});
+
+const actions = {
+  incrementCounter,
+  decrementCounter
+}
+
+class TestComponent extends Component {
+  render() {
+const {incrementCounter, decrementCounter, data} = this.props;
+
+    return (
+      <div>
+        <h1>Test Component</h1>
+        <h3>The Answer is {data}</h3>
+        <Button onClick={decrementCounter} color="red" content="Decrement"/>
+        <Button onClick={incrementCounter} color="green" content="Increment"/>
+
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, actions)(TestComponent);
